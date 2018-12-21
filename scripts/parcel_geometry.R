074376f63b52e5d0b5631329fb03d7567c4d0fac
Conor Tompkins <conorotompkins@gmail.com>
2018-12-21 16:16:26 -0500

    exploratory

---
+++ scripts/parcel_geometry.R
@@ -0,0 +1,27 @@
+library(tidyverse)
+library(sf)
+
+file <- "data/shapefiles/AlleghenyCounty_Parcels201812.shx"
+file
+parcels <- st_read(file)
+
+nrow(parcels)
+
+parcels <- parcels %>% 
+  mutate(valid = st_is_valid(.))
+
+parcels %>% 
+  ggplot(aes(CALCACREAG)) +
+  geom_density()
+
+parcels %>% 
+  ggplot(aes(Shape_Leng, Shape_Area)) +
+  geom_point()
+
+plot <- parcels %>% 
+  filter(valid) %>% 
+  ggplot() +
+  geom_sf(aes(fill = CALCACREAG)) +
+  scale_fill_viridis_c()
+
+plot
