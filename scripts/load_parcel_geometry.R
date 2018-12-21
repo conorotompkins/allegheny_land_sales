0292805f3b4fa30318523902e18e98247bd78050
Conor Tompkins <conorotompkins@gmail.com>
2018-12-21 16:16:06 -0500

    code for loading geometry

---
+++ scripts/load_parcel_geometry.R
@@ -0,0 +1,18 @@
+library(tidyverse)
+library(sf)
+library(lwgeom)
+
+file <- "data/shapefiles/AlleghenyCounty_Parcels201812.shx"
+file
+parcels <- st_read(file)
+
+nrow(parcels)
+
+#parcels <- parcels %>% 
+#  mutate(valid = st_is_valid(.))
+
+parcels <- parcels %>% 
+  st_make_valid()
+
+parcels <- parcels %>% 
+  mutate(valid = st_is_valid(.))
