9d6d95bf8d1f26a6fd6a287c3882b1c63d9f3075
Conor Tompkins <conorotompkins@gmail.com>
2018-12-24 17:57:11 -0500

    cleaned up code, added viz

--- scripts/price_model.R
+++ scripts/price_model.R
@@ -2,19 +2,34 @@ library(tidyverse)
 library(janitor)
 library(broom)
 library(modelr)
+library(scales)
+library(caret)
 
-options(scipen = 999, digits = 4)
+options(scipen = 999, digits = 5)
 
 theme_set(theme_bw())
 
-df <- read_csv("data/parcel_exploratory.csv", progress = FALSE)
+df <- read_csv("data/parcel_exploratory.csv", progress = FALSE) %>%
+  select(calcacreag, schooldesc, munidesc, instrtypdesc, price) %>% 
+  filter(instrtypdesc == "DEED") %>% 
+  na.omit()
 
-fit <- lm(price ~ calcacreag + instrtypdesc + schooldesc, data = df)
+summary(df)
+
+fit <- lm(price ~ calcacreag + schooldesc, data = df)
 
 model <- fit %>%
   tidy() %>% 
   arrange(desc(estimate))
+
 model
+
+model %>% 
+  mutate(term = fct_reorder(term, estimate)) %>% 
+  ggplot(aes(term, estimate)) +
+  geom_point() +
+  coord_flip() +
+  scale_y_continuous(labels = dollar)
 
 fit %>% 
   glance()
@@ -30,4 +45,5 @@ rmse(fit, df)
 
 sqrt(mean(error^2))
 ?rmse
+
 
