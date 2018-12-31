912c06b49977f34dad580ef95a4778c390f917f6
Conor Tompkins <conorotompkins@gmail.com>
2018-12-31 13:27:21 -0500

    changed modeling code to log10 tranformation variables

--- scripts/house_sales_model.R
+++ scripts/house_sales_model.R
@@ -11,23 +11,30 @@ source("scripts/load_parcel_sales_combined.R")
 df <- df %>% 
   filter(classdesc_asmt == "RESIDENTIAL",
          saleprice_asmt > 100,
-         str_detect(munidesc_asmt, "Ward")) %>% 
+         str_detect(munidesc_asmt, "Ward"),
+         finishedlivingarea_asmt > 0,
+         lotarea_asmt > 0) %>% 
   select(pin, munidesc_asmt, schooldesc_asmt, neighdesc_asmt, taxdesc_asmt,
          usedesc_asmt, homesteadflag_asmt, farmsteadflag_asmt, styledesc_asmt,
          yearblt_asmt, extfinish_desc_asmt, roofdesc_asmt,  basementdesc_asmt,
          gradedesc_asmt, conditiondesc_asmt, stories_asmt, totalrooms_asmt, bedrooms_asmt,
          fullbaths_asmt, halfbaths_asmt, heatingcoolingdesc_asmt, fireplaces_asmt, 
-         bsmtgarage_asmt, finishedlivingarea_asmt, lotarea_asmt, price_sales, saleprice_asmt,
-         saledate_sales)
+         bsmtgarage_asmt, finishedlivingarea_asmt_log10, lotarea_asmt_log10, price_sales_log10, 
+         saleprice_asmt_log10, saledate_sales)
 
 df <- df %>% 
   mutate_if(is.character, replace_na, "missing") %>% 
   mutate_if(is.character, as.factor)
 
+glimpse(df)
+
 
 df <- df %>% 
-  select(munidesc_asmt, bedrooms_asmt, saleprice_asmt) %>% 
+  select(munidesc_asmt, yearblt_asmt, bedrooms_asmt, fullbaths_asmt, halfbaths_asmt, finishedlivingarea_asmt_log10, 
+         lotarea_asmt_log10, saleprice_asmt_log10) %>% 
   na.omit()
+
+glimpse(df)
 
 #df <- df %>% 
 #  group_by(munidesc_asmt) %>% 
@@ -43,12 +50,12 @@ df <- df %>%
 #findLinearCombos(df)
 
 model <- train(
-  saleprice_asmt ~ ., df,
+  saleprice_asmt_log10 ~ ., df,
   method = "lm",
   trControl = trainControl(
     method = "repeatedcv", number = 5, repeats = 5, verboseIter = TRUE
   )
 )
 model
-warnings()
-
+varImp(model) %>% 
+  typeof()
