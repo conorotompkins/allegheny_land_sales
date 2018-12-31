8540156a8ed3793b460d2e23a7ec5d17606ec1d5
Conor Tompkins <conorotompkins@gmail.com>
2018-12-31 13:44:16 -0500

    added more variables, added varImp code

--- scripts/house_sales_model.R
+++ scripts/house_sales_model.R
@@ -16,9 +16,9 @@ df <- df %>%
          lotarea_asmt > 0) %>% 
   select(pin, munidesc_asmt, schooldesc_asmt, neighdesc_asmt, taxdesc_asmt,
          usedesc_asmt, homesteadflag_asmt, farmsteadflag_asmt, styledesc_asmt,
-         yearblt_asmt, extfinish_desc_asmt, roofdesc_asmt,  basementdesc_asmt,
-         gradedesc_asmt, conditiondesc_asmt, stories_asmt, totalrooms_asmt, bedrooms_asmt,
-         fullbaths_asmt, halfbaths_asmt, heatingcoolingdesc_asmt, fireplaces_asmt, 
+         yearblt_asmt, extfinish_desc_asmt, roofdesc_asmt,  basementdesc_asmt, 
+         heatingcoolingdesc_asmt, gradedesc_asmt, conditiondesc_asmt, stories_asmt, 
+         totalrooms_asmt, bedrooms_asmt, fullbaths_asmt, halfbaths_asmt, fireplaces_asmt, 
          bsmtgarage_asmt, finishedlivingarea_asmt_log10, lotarea_asmt_log10, price_sales_log10, 
          saleprice_asmt_log10, saledate_sales)
 
@@ -30,8 +30,10 @@ glimpse(df)
 
 
 df <- df %>% 
-  select(munidesc_asmt, yearblt_asmt, bedrooms_asmt, fullbaths_asmt, halfbaths_asmt, finishedlivingarea_asmt_log10, 
-         lotarea_asmt_log10, saleprice_asmt_log10) %>% 
+  select(neighdesc_asmt, finishedlivingarea_asmt_log10, lotarea_asmt_log10,
+         yearblt_asmt, bedrooms_asmt, fullbaths_asmt, halfbaths_asmt, 
+         extfinish_desc_asmt, roofdesc_asmt, basementdesc_asmt, heatingcoolingdesc_asmt,
+         gradedesc_asmt, conditiondesc_asmt, saleprice_asmt_log10) %>% 
   na.omit()
 
 glimpse(df)
@@ -57,5 +59,4 @@ model <- train(
   )
 )
 model
-varImp(model) %>% 
-  typeof()
+varImp(model)
