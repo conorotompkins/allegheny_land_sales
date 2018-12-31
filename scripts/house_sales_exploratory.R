e970c276df6438eb932074ad25be036b394d9883
Conor Tompkins <conorotompkins@gmail.com>
2018-12-31 13:11:17 -0500

    added log10 transformations for price and size variables

--- scripts/house_sales_exploratory.R
+++ scripts/house_sales_exploratory.R
@@ -11,13 +11,20 @@ df <- df %>%
   filter(classdesc_asmt == "RESIDENTIAL",
          str_detect(munidesc_asmt, "Ward"),
          saleprice_asmt > 1) %>% 
+  mutate(finishedlivingarea_asmt_log10 = log10(finishedlivingarea_asmt),
+         lotarea_asmt_log10 = log10(lotarea_asmt),
+         price_sales_log10 = log10(price_sales),
+         saleprice_asmt_log10 = log10(saleprice_asmt)) %>% 
   select(pin, munidesc_asmt, schooldesc_asmt, neighdesc_asmt, taxdesc_asmt,
          usedesc_asmt, homesteadflag_asmt, farmsteadflag_asmt, styledesc_asmt,
          yearblt_asmt, extfinish_desc_asmt, roofdesc_asmt,  basementdesc_asmt,
          gradedesc_asmt, conditiondesc_asmt, stories_asmt, totalrooms_asmt, bedrooms_asmt,
          fullbaths_asmt, halfbaths_asmt, heatingcoolingdesc_asmt, fireplaces_asmt, 
-         bsmtgarage_asmt, finishedlivingarea_asmt, lotarea_asmt, price_sales, saleprice_asmt,
-         saledate_sales)
+         bsmtgarage_asmt, finishedlivingarea_asmt, finishedlivingarea_asmt_log10,
+         lotarea_asmt, lotarea_asmt_log10, saledate_sales, price_sales, price_sales_log10, 
+         saleprice_asmt, saleprice_asmt_log10)
+  
+glimpse(df)
 
 n_ntile <- 40
 
@@ -54,8 +61,7 @@ df %>%
   geom_density()
 
 df %>% 
-  count(extfinish_desc_asmt, sort = TRUE) %>% 
-  View()
+  count(extfinish_desc_asmt, sort = TRUE)
 
 df %>% 
   count(roofdesc_asmt, sort = TRUE)
@@ -94,52 +100,39 @@ df %>%
   count(bsmtgarage_asmt, sort = TRUE)
 
 df %>% 
-  select(finishedlivingarea_asmt) %>% 
-  mutate(q = ntile(finishedlivingarea_asmt, n_ntile)) %>% 
-  filter(q < n_ntile) %>% 
-  ggplot(aes(finishedlivingarea_asmt)) +
+  select(finishedlivingarea_asmt_log10) %>% 
+  ggplot(aes(finishedlivingarea_asmt_log10)) +
   geom_density()
 
 df %>% 
-  select(lotarea_asmt) %>% 
-  mutate(q = ntile(lotarea_asmt, n_ntile)) %>% 
-  filter(q < n_ntile) %>% 
-  ggplot(aes(lotarea_asmt)) +
+  select(lotarea_asmt_log10) %>% 
+  ggplot(aes(lotarea_asmt_log10)) +
   geom_density()
 
 df %>% 
-  select(lotarea_asmt, finishedlivingarea_asmt) %>% 
-  na.omit() %>% 
-  mutate(lot_ntile = ntile(lotarea_asmt, n_ntile),
-         finished_ntile = ntile(finishedlivingarea_asmt, n_ntile)) %>% 
-  filter(lotarea_asmt < 1000000) %>% 
-  #filter(lot_ntile < n_ntile) %>% 
-  #count(lotarea_asmt, finishedlivingarea_asmt) %>% 
-  ggplot(aes(lotarea_asmt, finishedlivingarea_asmt)) +
+  select(lotarea_asmt_log10, finishedlivingarea_asmt_log10) %>% 
+  ggplot(aes(lotarea_asmt_log10, finishedlivingarea_asmt_log10)) +
+  stat_density_2d(aes(fill = stat(level)), geom = "polygon") +
+  scale_fill_viridis_c() +
+  coord_cartesian(xlim = c(2.75, 4.25),
+                  ylim = c(2.75, 4.25))
+
+df %>% 
+  select(price_sales_log10) %>% 
+  ggplot(aes(price_sales_log10)) +
+  geom_density()
+
+df %>% 
+  select(saleprice_asmt_log10) %>% 
+  ggplot(aes(saleprice_asmt_log10)) +
+  geom_density()
+
+df %>% 
+  select(saleprice_asmt_log10, price_sales_log10) %>% 
+  ggplot(aes(saleprice_asmt_log10, price_sales_log10)) +
+  #geom_point() +
   stat_density_2d(aes(fill = stat(level)), geom = "polygon") +
   scale_fill_viridis_c()
-
-df %>% 
-  select(price_sales) %>% 
-  #filter(price_sales > 1000) %>% 
-  mutate(price_ntile = ntile(price_sales, n_ntile)) %>% 
-  filter(price_ntile < n_ntile) %>% 
-  ggplot(aes(price_sales)) +
-  geom_density() +
-  scale_x_continuous(labels = dollar)
-
-df %>% 
-  select(saleprice_asmt) %>% 
-  mutate(price_ntile = ntile(saleprice_asmt, n_ntile)) %>% 
-  filter(price_ntile < n_ntile) %>% 
-  ggplot(aes(saleprice_asmt)) +
-  geom_density() +
-  scale_x_continuous(labels = dollar)
-
-df %>% 
-  select(saleprice_asmt, price_sales) %>% 
-  ggplot(aes(saleprice_asmt, price_sales)) +
-  geom_point()
 
 df %>% 
   count(saledate_sales, sort = TRUE) %>% 
@@ -185,33 +178,33 @@ df %>%
   theme(panel.grid = element_blank())
 
 df %>% 
-  select(finishedlivingarea_asmt, totalrooms_asmt) %>% 
-  ggplot(aes(finishedlivingarea_asmt, totalrooms_asmt)) +
+  select(finishedlivingarea_asmt_log10, totalrooms_asmt) %>% 
+  ggplot(aes(finishedlivingarea_asmt_log10, totalrooms_asmt)) +
   stat_density_2d(aes(fill = stat(level)), geom = "polygon") +
   scale_fill_viridis_c()
 
 df %>% 
-  select(finishedlivingarea_asmt, bedrooms_asmt) %>% 
-  ggplot(aes(finishedlivingarea_asmt, bedrooms_asmt)) +
+  select(finishedlivingarea_asmt_log10, bedrooms_asmt) %>% 
+  ggplot(aes(finishedlivingarea_asmt_log10, bedrooms_asmt)) +
   stat_density_2d(aes(fill = stat(level)), geom = "polygon") +
   scale_fill_viridis_c()
 
 df %>% 
-  select(finishedlivingarea_asmt, fullbaths_asmt) %>% 
-  ggplot(aes(finishedlivingarea_asmt, fullbaths_asmt)) +
+  select(finishedlivingarea_asmt_log10, fullbaths_asmt) %>% 
+  ggplot(aes(finishedlivingarea_asmt_log10, fullbaths_asmt)) +
   stat_density_2d(aes(fill = stat(level)), geom = "polygon") +
   scale_fill_viridis_c() +
   scale_y_continuous(breaks = c(0:3))
 
 df %>% 
-  select(finishedlivingarea_asmt, halfbaths_asmt) %>% 
-  ggplot(aes(finishedlivingarea_asmt, halfbaths_asmt)) +
+  select(finishedlivingarea_asmt_log10, halfbaths_asmt) %>% 
+  ggplot(aes(finishedlivingarea_asmt_log10, halfbaths_asmt)) +
   stat_density_2d(aes(fill = stat(level)), geom = "polygon") +
   scale_fill_viridis_c() +
   scale_y_continuous(breaks = c(0:3))
 
 df %>% 
-  ggplot(aes(yearblt_asmt, saleprice_asmt, color = bedrooms_asmt)) +
+  ggplot(aes(yearblt_asmt, saleprice_asmt_log10, color = bedrooms_asmt)) +
   geom_point(aes(size = bedrooms_asmt), alpha = .1) +
   geom_smooth() +
   scale_color_viridis_c()
@@ -226,21 +219,20 @@ df %>%
   theme(panel.grid = element_blank())
 
 df %>% 
-  filter(saleprice_asmt < 2000000) %>% 
-  ggplot(aes(finishedlivingarea_asmt, saleprice_asmt)) +
+  ggplot(aes(finishedlivingarea_asmt_log10, saleprice_asmt_log10)) +
   geom_point(alpha = .1) +
   geom_smooth(method = "lm")
 
 df %>% 
-  select(munidesc_asmt, saleprice_asmt) %>%
-  filter(saleprice_asmt < 20000000) %>% 
+  select(munidesc_asmt, saleprice_asmt_log10) %>%
   group_by(munidesc_asmt) %>% 
-  mutate(median_price = median(saleprice_asmt, na.rm = TRUE)) %>% 
+  mutate(median_price = median(saleprice_asmt_log10, na.rm = TRUE)) %>% 
   ungroup() %>% 
   mutate(munidesc_asmt = fct_reorder(munidesc_asmt, median_price)) %>%
-  ggplot(aes(munidesc_asmt, saleprice_asmt)) +
-  geom_jitter(alpha = .1) +
-  scale_y_continuous(labels = dollar) +
+  ggplot(aes(munidesc_asmt, saleprice_asmt_log10)) +
+  #geom_jitter(alpha = .1) +
+  geom_boxplot() +
+  #scale_y_continuous(labels = dollar) +
   coord_flip()
 
 
