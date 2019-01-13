a0d487a0cf64eb7427337bb9a8ad5854016987dd
Conor Tompkins <conorotompkins@gmail.com>
2019-01-13 15:31:37 -0500

    annotated code, changed geom_jitter to geom_density_2d

--- scripts/model_cv.R
+++ scripts/model_cv.R
@@ -1,15 +1,17 @@
+#load libraries
 library(tidyverse)
 library(scales)
 library(caret)
 library(broom)
 library(modelr)
 library(rsample)
-library(Metrics)
 
+#set up environment
 options(scipen = 999, digits = 5)
 
 theme_set(theme_bw())
 
+#read in data
 source("scripts/load_parcel_sales_combined.R")
 
 #create grade vectors
@@ -23,6 +25,7 @@ grades_below_average_or_worse <- c("poor -", "poor", "poor +",
 grades_excellent_or_better <- c("excellent -", "excellent", "excellent +",
                                 "highest cost -", "highest cost", "highest cost +")
 
+#subset and engineer features
 df <- df %>% 
   filter(classdesc_asmt == "RESIDENTIAL",
          saleprice_asmt > 100,
@@ -47,53 +50,54 @@ df <- df %>%
                                                         "good -", "good", "good +",
                                                         "very good -", "very good", "very good +", "excellent - or better")))
 
+#replace missing character rows with "missing", change character columns to factor
 df <- df %>% 
   mutate_if(is.character, replace_na, "missing") %>% 
   mutate_if(is.character, as.factor)
 
-glimpse(df)
-
-
+#select response and features
 df <- df %>% 
   select(munidesc_asmt, usedesc_asmt, styledesc_asmt, conditiondesc_asmt, gradedesc_asmt,
          finishedlivingarea_asmt_log10, lotarea_asmt_log10, yearblt_asmt, bedrooms_asmt, 
          fullbaths_asmt, halfbaths_asmt, saleprice_asmt_log10) %>% 
-  #select_if(is.numeric) %>% 
   na.omit()
 
+#view data
 glimpse(df)
 
-# Prepare the initial split object
+#create initial split object
 df_split <- initial_split(df, prop = .75)
 df_split
-# Extract the training dataframe
+
+#extract training dataframe
 training_data_full <- training(df_split)
 training_data_full
 
-# Extract the testing dataframe
+#extract testing dataframe
 testing_data <- testing(df_split)
 testing_data
-# Calculate the dimensions of both training_data and testing_data
+
+#find dimensions of training_data_full and testing_data
 dim(training_data_full)
 dim(testing_data)
 
 set.seed(42)
 
-# Prepare the dataframe containing the cross validation partitions
+#prep the df with the cross validation partitions
 cv_split <- vfold_cv(training_data_full, v = 5)
 
 cv_data <- cv_split %>% 
   mutate(
-    # Extract the train dataframe for each split
+    #extract train dataframe for each split
     train = map(splits, ~training(.x)), 
-    # Extract the validate dataframe for each split
+    #extract validate dataframe for each split
     validate = map(splits, ~testing(.x))
   )
 
-# Use head() to preview cv_data
-head(cv_data)
+#view df
+cv_data
 
-# Build a model using the train data for each fold of the cross validation
+#build model using the train data for each fold of the cross validation
 cv_models_lm <- cv_data %>% 
   mutate(model = map(train, ~lm(formula = saleprice_asmt_log10 ~ ., data = .x)))
 cv_models_lm
@@ -102,30 +106,31 @@ cv_models_lm
 
 cv_prep_lm <- cv_models_lm %>% 
   mutate(
-    # Extract the recorded life expectancy for the records in the validate dataframes
+    #extract recorded life expectancy for the records in the validate dataframes
     validate_actual = map(validate, ~.x$saleprice_asmt_log10),
-    # Predict life expectancy for each validate set using its corresponding model
+    #predict response variable for each validate set using its corresponding model
     validate_predicted = map2(.x = model, .y = validate, ~predict(.x, .y))
   )
 
-head(cv_prep_lm)
+#View data
+cv_prep_lm
 
-# Calculate the mean absolute error for each validate fold       
+#calculate fit metrics for each validate fold       
 cv_eval_lm <- cv_prep_lm %>% 
-  mutate(validate_mae = map2_dbl(validate_actual, validate_predicted, ~Metrics::mae(actual = .x, predicted = .y)),
-         validate_rmse = map2_dbl(model, validate, modelr::rmse))
+  mutate(validate_rmse = map2_dbl(model, validate, modelr::rmse),
+         validate_mae = map2_dbl(model, validate, modelr::mae))
 
-cv_eval_lm %>% 
+cv_eval_lm <- cv_eval_lm %>% 
   mutate(fit = map(model, ~glance(.x))) %>% 
   unnest(fit)
 
-head(cv_eval_lm)
+#view data
+cv_eval_lm
 
-# Print the validate_mae column
-cv_eval_lm$validate_mae
-
-# Calculate the mean of validate_mae column
-mean(cv_eval_lm$validate_mae)
+#summarize fit metrics
+cv_eval_lm %>% 
+  select(validate_mae, validate_rmse, adj.r.squared) %>% 
+  summarize_all(mean)
 
 #glance at statistics for validate data
 cv_prep_lm %>% 
@@ -151,6 +156,7 @@ tidy_validate <- tidy(model) %>%
 tidy_validate %>% 
   filter(str_detect(term, "10th"))
 
+#visualize estimates for terms
 tidy_validate %>% 
   filter(term != "(Intercept)") %>% 
   mutate(term = fct_reorder(term, estimate)) %>% 
@@ -162,29 +168,34 @@ tidy_validate %>%
 augment_validate <- augment(model, newdata = validate_data) %>% 
   mutate(.resid = saleprice_asmt_log10 - .fitted)
 
+#actual vs. fitted
 cv_prep_lm %>% 
   unnest(validate_actual, validate_predicted) %>% 
   ggplot(aes(validate_actual, validate_predicted)) +
-  geom_jitter(alpha = .1) +
-  geom_smooth(method = "lm") +
   geom_abline() +
+  #geom_jitter(alpha = .1) +
+  stat_density_2d(aes(fill = stat(level)), geom = "polygon") +
+  geom_smooth(method = "lm") +
   scale_x_continuous(limits = c(2, 7)) +
   scale_y_continuous(limits = c(2, 7)) +
-  coord_equal()
+  coord_equal() +
+  scale_fill_viridis_c()
 
+#distribution of residuals
 augment_validate %>% 
   ggplot(aes(.resid)) +
   geom_density() +
   geom_vline(xintercept = 0, color = "red", linetype = 2)
 
+#sale price vs. residuals
 augment_validate %>% 
   ggplot(aes(.resid, saleprice_asmt_log10)) +
-  geom_jitter(alpha = .1) +
-  geom_vline(xintercept = 0, color = "red", linetype = 2)
+  #geom_jitter(alpha = .1) +
+  stat_density_2d(aes(fill = stat(level)), geom = "polygon") +
+  geom_vline(xintercept = 0, color = "red", linetype = 2) +
+  scale_fill_viridis_c()
 
-#calculate fit of test data
-head(testing_data)
-
+#calculate fit of model on test data
 modelr::rmse(model, testing_data)
 modelr::mae(model, testing_data)
 modelr::rsquare(model, testing_data)
@@ -192,24 +203,30 @@ modelr::rsquare(model, testing_data)
 #visualize model on test data
 test_augment <- augment(model, newdata = testing_data)
 
+#actual vs. fitted
 test_augment %>% 
   ggplot(aes(saleprice_asmt_log10, .fitted)) +
-  geom_jitter(alpha = .1) +
-  geom_smooth(method = "lm") +
   geom_abline() +
+  #geom_jitter(alpha = .1) +
+  stat_density_2d(aes(fill = stat(level)), geom = "polygon") +
+  geom_smooth(method = "lm") +
   scale_x_continuous(limits = c(2, 7)) +
   scale_y_continuous(limits = c(2, 7)) +
-  coord_equal()
+  coord_equal() +
+  scale_fill_viridis_c()
 
+#distribution of residuals
 test_augment %>% 
   mutate(.resid = saleprice_asmt_log10 - .fitted) %>% 
   ggplot(aes(.resid)) +
   geom_density() +
   geom_vline(xintercept = 0, color = "red", linetype = 2)
 
+#sale price vs. residuals
 test_augment %>% 
   mutate(.resid = saleprice_asmt_log10 - .fitted) %>% 
   ggplot(aes(.resid, saleprice_asmt_log10)) +
-  geom_jitter(alpha = .1) +
-  geom_vline(xintercept = 0, color = "red", linetype = 2)
-
+  #geom_jitter(alpha = .1) +
+  stat_density_2d(aes(fill = stat(level)), geom = "polygon") +
+  geom_vline(xintercept = 0, color = "red", linetype = 2) +
+  scale_fill_viridis_c()
