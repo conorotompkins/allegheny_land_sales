f1db929e0f755f59121230ce25f8603b29067f5b
Conor Tompkins <conorotompkins@gmail.com>
2019-03-24 22:44:45 -0400

    created script to create gif of houses being built

--- scripts/exploratory_mapping.R
+++ scripts/exploratory_mapping.R
@@ -8,20 +8,40 @@ options(scipen = 999, digits = 4)
 
 theme_set(theme_bw())
 
-source("scripts/load_parcel_sales.R")
+source("scripts/load_parcel_sales_combined.R")
 source("scripts/load_parcel_geometry.R")
 
 parcel_geometry <- parcels %>% 
-  inner_join(df, by = c("PIN" = "parid"))
+  inner_join(df, by = c("PIN" = "pin"))
 
 centroids <- parcel_geometry %>% 
   st_centroid() %>% 
   st_coordinates() %>% 
   as_tibble()
 
-parcel_geometry <- bind_cols(parcel_geometry, centroids) %>% 
+centroids %>% 
+  ggplot(aes(X, Y)) +
+  geom_point()
+
+parcel_geometry_cleaned <- bind_cols(parcel_geometry, centroids) %>% 
   clean_names() %>% 
-  distinct()
+  select(pin, x, y, yearblt_asmt, classdesc_asmt) %>% 
+  filter(classdesc_asmt == "RESIDENTIAL",
+         !is.na(yearblt_asmt)) %>% 
+  st_set_geometry(NULL)
+
+plot1 <- parcel_geometry_cleaned %>% 
+  ggplot(aes(x, y, color = yearblt_asmt)) +
+  geom_point(alpha = .3, size = .1) +
+  scale_color_viridis_c("Year house was built") +
+  theme_void() +
+  theme(axis.text = element_blank(),
+        axis.title = element_blank()) +
+  labs(title = "City of Pittsburgh housing parcels",
+       subtitle = "Residential parcels",
+       caption = "@conor_tompkins, data from @WPRDC")
+#plot1
+ggsave("output/images/pittsburgh_parcels_year_built.png", plot1, height = 12, width = 12, dpi = 300)
 
 plot <- parcel_geometry %>% 
   filter(instrtypdesc == "DEED") %>% 
