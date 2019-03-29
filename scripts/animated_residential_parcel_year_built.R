438dcbe0cdae499c557daf6dd85dbaeddbd9b8c1
Conor Tompkins <conorotompkins@gmail.com>
2019-03-29 18:16:00 -0400

    copied over code to read in parcel data, filtered out bad year data

--- scripts/animated_residential_parcel_year_built.R
+++ scripts/animated_residential_parcel_year_built.R
@@ -9,7 +9,16 @@ options(scipen = 999, digits = 4)
 
 theme_set(theme_bw())
 
-source("scripts/load_parcel_sales_combined.R")
+#read in data
+df <- read_csv("data/parcel_data_allegheny_county.csv", progress = FALSE) %>% 
+  clean_names() %>% 
+  select(-geom)
+
+df %>% 
+  ggplot(aes(yearblt_asmt)) +
+  geom_density() +
+  geom_rug()
+
 source("scripts/load_parcel_geometry.R")
 
 parcel_geometry <- parcels %>% 
@@ -26,22 +35,24 @@ centroids %>%
 
 parcel_geometry_cleaned <- bind_cols(parcel_geometry, centroids) %>% 
   clean_names() %>% 
-  select(pin, x, y, yearblt_asmt, classdesc_asmt) %>%
+  select(pin, x, y, yearblt_asmt) %>%
   mutate(yearblt_asmt = as.integer(yearblt_asmt)) %>% 
-  filter(classdesc_asmt == "RESIDENTIAL",
-         !is.na(yearblt_asmt)) %>% 
+  filter(!is.na(yearblt_asmt),
+         yearblt_asmt > 1800) %>% 
   st_set_geometry(NULL)
-
 
 p <- parcel_geometry_cleaned %>% 
   ggplot(aes(x, y, color = yearblt_asmt, group = pin)) +
   geom_point(alpha = .3, size = .1) +
-  scale_color_viridis_c("Year house was built") +
+  scale_color_viridis_c("Year structure was built") +
   theme_void() +
   theme(axis.text = element_blank(),
         axis.title = element_blank()) +
-  labs(title = "City of Pittsburgh residential housing parcels",
+  labs(title = "Allegheny County land parcels",
        subtitle = "Year built: {frame_along}",
        caption = "@conor_tompkins, data from @WPRDC")
 p <- p + transition_reveal(yearblt_asmt)
-anim_save("output/images/pittsburgh_residential_parcels_yearblt.gif")
+
+anim_save("output/images/pittsburgh_all_parcels_yearblt.gif", p)
+
+p
