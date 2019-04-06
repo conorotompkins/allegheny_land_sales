f9d9e511f4a94e1a9b032abf29d08a2d4a7df187
Conor Tompkins <conorotompkins@gmail.com>
2019-04-06 11:53:50 -0400

    updated script for mapping parcel geometry

--- scripts/parcel_geometry.R
+++ scripts/parcel_geometry.R
@@ -1,28 +1,37 @@
 library(tidyverse)
 library(sf)
+library(gganimate)
 
-file <- "data/shapefiles/AlleghenyCounty_Parcels201812.shx"
-file
-parcels <- st_read(file)
+theme_set(theme_bw())
+
+#file <- "data/shapefiles/AlleghenyCounty_Parcels201812.shx"
+#file
+#parcels <- st_read(file)
+
+source("scripts/load_parcel_sales_combined.R")
+source("scripts/load_parcel_geometry.R")
+
+parcel_geometry <- parcels %>% 
+  left_join(df, by = c("PIN" = "pin"))
 
 nrow(parcels)
 
 parcels <- parcels %>% 
   mutate(valid = st_is_valid(.))
 
-parcels %>% 
-  ggplot(aes(CALCACREAG)) +
-  geom_density()
+#parcels %>% 
+#  ggplot(aes(CALCACREAG)) +
+#  geom_density()
 
-parcels %>% 
-  ggplot(aes(Shape_Leng, Shape_Area)) +
-  geom_point()
+#parcels %>% 
+#  ggplot(aes(Shape_Leng, Shape_Area)) +
+#  geom_point()
 
-plot <- parcels %>% 
+plot <- parcel_geometry %>% 
+  select(valid, yearblt_asmt) %>% 
   filter(valid,
-         CALCACREAG > 50) %>% 
+         !is.na(yearblt_asmt)) %>% 
   ggplot() +
-  geom_sf(aes(fill = CALCACREAG)) +
-  scale_fill_viridis_c()
+  geom_sf()
 
 plot
